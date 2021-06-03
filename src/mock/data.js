ad8c9e6597b8489f7b70c9186ae213346f2b7820
gambo94 <fedigambo94@gmail.com>
2021-06-03 12:27:30 +0200

    uploading portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,57 +9,59 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is',
+  name: 'Federico',
+  subtitle: "I'm a Fullstack Web Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'myprofile.jpg',
+  paragraphOne:
+    'After completing a Master’s degree in Translation Technology at the Universidad Autònoma de Barcelona and working as a Project Manager, I have decided to take a deep dive into the world of coding myself.',
+  paragraphTwo: 'I love languages, travelling, videogames and mangas',
+  paragraphThree: '“Experience is the name everyone gives to their mistakes.”       - Oscar Wilde ',
+  resume: 'https://www.dropbox.com/s/jfp4i4pqggneh1j/Federico%20Gambini_EN.pdf?dl=0', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project1.jpg',
+    title: 'Face detection app',
+    info: 'A web app that detects a face of a given picture. Built with React, NodeJS, mySQL and deployed on Heroku. Give it a try!',
+    info2:
+      'You will need to sign up, copy and paste the link of your desired picture and most of all, have fun!',
+    url: 'https://face-detector-react-node.herokuapp.com/',
+    repo: 'https://github.com/gambo94/face-detection-app', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'chat.jpg',
+    title: 'Real time chat',
+    info: 'A realtime chat app built with Socket.io, Node.js, typeORM and VanillaJS',
+    info2: 'It includes a sign up form, log in and rooms selection',
+    // url: '',
+    repo: 'https://github.com/gambo94/chat-websocket', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'dice.jpg',
+    title: 'A dice game restful API',
+    info: 'An API that could support a dice game with various endpoints',
+    info2: 'Built using NodeJS, MongoDB and JWT authentication',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/gambo94/dice-game-API', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Would you like to talk with me?',
+  btn: 'Email me',
+  email: 'fedigambo94@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +69,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/fgambini/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/gambo94',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
